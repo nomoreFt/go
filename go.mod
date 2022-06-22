bbc0b22552a783b7d325ad781903e4b5b4df537e
woodiePlanit <woodie@planit.ai>
2022-06-22 18:17:18 +0900

    commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-cp
+
+go 1.17
+
+require github.com/nmrshll/go-cp v0.0.0-20180115193924-61436d3b7cfa
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -9,31 +10,52 @@ import (
 
 func main() {
 
-	targetDir := "D://test/moveDir"
-	newDir := "D://test/dir"
+	srcDir := "D://test/moveDir"
+	destDir := "D://test/dir"
 
-	c := make(chan string)
+	//c := make(chan string)
 
-	files, err := ioutil.ReadDir(targetDir)
+	files, err := ioutil.ReadDir(srcDir)
 	if err != nil {
 		fmt.Println(err)
 	}
 	for _, file := range files {
-		go moveFile(file.Name(), targetDir, newDir, c)
+		//go moveFile(file.Name(), srcDir, destDir, c)
+		file.Name()
+		copyFile(srcDir, destDir)
 
 	}
-	for i := 0; i < len(files); i++ {
+	/*	for i := 0; i < len(files); i++ {
 		result := <-c
 		fmt.Println(result)
-	}
-
+	}*/
 }
 
-func moveFile(fileNm string, targetDir string, newDir string, c chan string) {
+func copyFile(srcDir, destDir string) {
+	fin, err := os.Open(srcDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fin.Close()
 
-	oldLocation := targetDir + "/" + fileNm
-	newLocation := newDir + "/" + fileNm
+	fout, err := os.Create(destDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fout.Close()
+
+	_, err = io.Copy(fout, fin)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func moveFile(fileNm string, srcDir string, destDir string, c chan string) {
+
+	oldLocation := srcDir + "/" + fileNm
+	newLocation := destDir + "/" + fileNm
 	err := os.Rename(oldLocation, newLocation)
+
 	if err != nil {
 		log.Fatal(err)
 	}
